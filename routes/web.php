a4eb53c703311cc83e52cb46fb5015535e21f077
firmaaaaan <firmansyah@unisayogya.ac.id>
2024-05-20 13:38:38 +0700

    add : login page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/dashboard-admin', function () {
     return view('components.admin.dashboard-admin');
 });
+
+Route::get('/', [AuthController::class, 'login'])->name('login');
